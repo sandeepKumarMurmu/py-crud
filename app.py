14d8d1c382d55a18c6258dc8ca37cefe059de8c0
ALLEN-SandeepKumarMurmu <90225941+sandeepKumarMurmu@users.noreply.github.com>
2025-09-09 06:02:33 +0530

    ADD : Crud learning initiated

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify, request
+import datetime
+import jwt
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -12,6 +14,21 @@ def home():
 def greet():
     name = request.args.get('name', 'Guest')
     return jsonify({"message": f"Hello, {name}!"})
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if not data:  # None or empty dict
+        return jsonify({"error": "Request body cannot be empty"}), 400
+    
+    username = data.get('username')
+    password = data.get('password')
+
+    if username == 'admin' and password == 'admin@123':
+        token = jwt.encode({"username":username,"password":password},"test@123", algorithm="HS256")
+        return jsonify({"token": token})
+    return jsonify({"message":"Unauthorized"}), 401
 
 if __name__ == '__main__':
     app.run(debug=True)
